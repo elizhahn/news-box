7b4d7ab36627fd5112f7686c8981564b979d5033
Elizabeth Hahn <elizhahn87@gmail.com>
2021-06-01 15:47:07 -0600

    Setup Article List component with App component

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import ArticleList from '../ArticleList/ArticleList';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [section, setSection] = useState('')
+  const [articles, setArticles] = useState([])
+  const [expanded, setExpanded] = useState(false)
+
   return (
     <div className="App">
       <header className="App-header">
       
       </header>
+      <ArticleList/>
     </div>
   );
 }
---
+++ src/ArticleList/ArticleList.js
@@ -0,0 +1,11 @@
+import { React } from "react"
+
+const ArticleList = ({ articles }) => {
+  return (
+    <main>
+      <p>articles go here</p>
+    </main>
+  )
+}
+
+export default ArticleList
