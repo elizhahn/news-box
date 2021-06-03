42013faa0b1c9cc1af74e792aa013db42d2cc068
Elizabeth Hahn <elizhahn87@gmail.com>
2021-06-03 09:30:59 -0600

    Add basic error handling to article list

--- src/ArticleList/ArticleList.js
+++ src/ArticleList/ArticleList.js
@@ -2,7 +2,7 @@ import { React } from "react";
 import ArticleCard from '../ArticleCard/ArticleCard';
 import './ArticleList.css';
 
-const ArticleList = ({ articles, showDetails, section }) => {
+const ArticleList = ({ articles, showDetails, section, err }) => {
   const articleList = articles.map((article, i) => {
     return <ArticleCard
               key={i}
@@ -16,6 +16,8 @@ const ArticleList = ({ articles, showDetails, section }) => {
     <main className="article-list-container">
       <h2 className="article-list-section">{`You are now viewing the ${section} section...`}</h2>
       <div className="article-list">
+        {!err && !articles.length && <h1 className="err-msg">Loading news...</h1>}
+        {err && <h1 className="err-msg">Sorry, we're having an issue grabbing today's news..check back later</h1>}
         { articleList }
       </div>
     </main>
